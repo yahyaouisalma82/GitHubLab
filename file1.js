0d51728844a8615ee112d538d6e842c53283ce4d
salma yahyaoui <yahyaouisalma14@gmail.com>
2023-09-28 14:35:58 +0100

    first commit

---
+++ file1.js
@@ -0,0 +1,18 @@
+const display = document.querySelector('.display');
+const clear = document.querySelector('.clear');
+const equal = document.querySelector('.equal');
+const numbers = document.querySelectorAll('.number');
+const buttons = document.querySelectorAll('.number, .operator');
+const operators = document.querySelectorAll('.operator');
+
+buttons.forEach(bouton => {
+    bouton.addEventListener('click', function() {
+        display.textContent += this.textContent;
+    });
+});
+equal.addEventListener('click', () => {
+    display.textContent = eval(display.textContent);
+})
+clear.addEventListener('click', () => {
+    display.textContent = '';
+})
